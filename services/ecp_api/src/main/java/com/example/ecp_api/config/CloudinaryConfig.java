047ee6d7d8f5bf450a4139a8aaad05fe4a963b80
Thane Nguyen <93665501+thanhnguyenhoang171@users.noreply.github.com>
2026-07-02 11:38:54 +0700

    fix: Error creating bean with name 'cloudinaryConfig': Injection of autowired dependencies failed

--- services/ecp_api/src/main/java/com/example/ecp_api/config/CloudinaryConfig.java
+++ services/ecp_api/src/main/java/com/example/ecp_api/config/CloudinaryConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class CloudinaryConfig {
+class CloudinaryConfig {
     @Value("${cloudinary.cloud-name}")
     private String cloudName;
     @Value("${cloudinary.api-key}")
