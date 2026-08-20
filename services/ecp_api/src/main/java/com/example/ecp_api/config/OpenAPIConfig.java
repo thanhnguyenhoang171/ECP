ff39e239f00ff0b65adf2c4d3e6b94158c1836e7
Thane Nguyen <93665501+thanhnguyenhoang171@users.noreply.github.com>
2026-08-20 17:22:37 +0700

    feat: configure application properties and define OpenAPI documentation structure

--- services/ecp_api/src/main/java/com/example/ecp_api/config/OpenAPIConfig.java
+++ services/ecp_api/src/main/java/com/example/ecp_api/config/OpenAPIConfig.java
@@ -6,9 +6,12 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenAPIConfig {
@@ -17,6 +20,9 @@ public class OpenAPIConfig {
     public OpenAPI customOpenAPI() {
         final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("/api").description("Default API URL")
+                ))
                 .info(new Info()
                         .title("ECP E-Commerce Platform API Documentation")
                         .version("1.0.0")
