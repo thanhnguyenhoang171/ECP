7e6c709308bd6d38b9bd679e1a63eab5752435a0
Thane Nguyen <93665501+thanhnguyenhoang171@users.noreply.github.com>
2026-08-20 17:01:41 +0700

    fix: wrong query in file migration

--- services/ecp_api/src/main/resources/db/mysql/migrations/V2__add_expected_delivery_date_to_purchase_orders.sql
+++ services/ecp_api/src/main/resources/db/mysql/migrations/V2__add_expected_delivery_date_to_purchase_orders.sql
@@ -1,4 +1,4 @@
 -- Migration: Add expected_delivery_date column to purchase_orders table
 ALTER TABLE purchase_orders 
-ADD COLUMN IF NOT EXISTS expected_delivery_date DATETIME NULL AFTER note;
+ADD COLUMN expected_delivery_date DATETIME NULL AFTER note;
 
